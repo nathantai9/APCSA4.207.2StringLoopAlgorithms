a27cff3aef78f4e29aa9ddc629e87facc90525a0
Nathan Tai <nathantai9@gmail.com>
2026-03-15 16:31:07 -0400

    TODO use VSCodium instead of VSCode

---
+++ instructions.md
@@ -0,0 +1,91 @@
+---
+layout: project
+category: 207_iteration
+title: StringLoopAlgorithms
+---
+Concepts: for loops, String methods, Integer.parseInt(String s)
+
+### String to int
+
+One of the methods on the AP CSA Quick Reference Sheet is Integer.parseInt()
+
+Since you're working with number strings (for example "243546" or "7"), remember that Integer.parseInt() expects a String argument and returns an int
+
+<table>
+<tr>
+<td>static int parseInt(String s)</td>
+<td>Returns the String argument as an int</td>
+</tr>
+</table>
+
+```
+// convert String to int
+// using Integer.parseInt()
+
+int number1 = Integer.parseInt("67");
+```
+
+### int to String
+
+
+
+The EASY way to convert an int to a String is concatenation with an empty String:
+
+```
+// convert an int to a String by
+// concatenating with empty String
+
+String str1 = "" + 67;
+```
+
+There is a static Integer class method BUT IT IS NOT ON THE AP EXAM. You do NOT have to use it. It is Integer.toString()... String str1 = Integer.toString(67);
+
+## Directions
+
+Pick any 5 of the following algorithms and write static methods for them (ask Mr. Riley if you need clarification about how an algorithm should work)
+
+<ul id="algorithmList">
+</ul>
+
+In the main method, create 5 expected/actual tests for EACH algorithm. Your finished project will have a total of 25 expected/actual tests. For example, if I created an algorithm String sayHelloName(String name), then my tests might look like this...
+
+```
+System.out.println("- - -Testing sayHelloName algorithm- - -");
+System.out.println("Expect: Hello Alice \nActual: " + sayHelloName("Alice"));
+System.out.println("Expect: Hello Bob \nActual: " + sayHelloName("Bob"));
+System.out.println("Expect: Hello Carol \nActual: " + sayHelloName("Carol"));
+System.out.println("Expect: Hello Dave \nActual: " + sayHelloName("Dave"));
+System.out.println("Expect: Hello Eliza \nActual: " + sayHelloName("Eliza"));
+System.out.println("- - -Testing the next algorithm- - -");
+```
+
+<script>
+let mylist = [
+    "String sayLaXTimes(int x)",
+    "int sumDigitsOfNumberString(String num)",
+    "double averageDigitsOfNumberString(String num)",
+    "int countTimesLetterAppears(String fullString, String letter)",
+    "boolean doesLetterAppear(String fullString, String letter)",
+    "boolean hasOnlyLetter(String fullString, String letter)",
+    "String removeLetter(String fullString, String letter)",
+    "boolean hasNumbers(String myString)",
+    "String replaceLetterWithStars(String fullString, String letter)"
+]
+mylist.push( Math.random() > 0.5 ? "int countConsonants(String myString)" : "int countVowels(String myString)");
+mylist.push( Math.random() > 0.5 ? "String removeConsonants(String myString)" : "String removeVowels(String myString)");
+mylist.push( Math.random() > 0.5 ? "int removeOddDigits(String number)" : "int removeEvenDigits(String number)");
+mylist.push( Math.random() > 0.5 ? "boolean hasOnlyConsonants(String myString)" : "boolean hasOnlyVowels(String myString)");
+mylist.push( Math.random() > 0.5 ? "int maxDigitOfNumberString(String num)" : "int minDigitOfNumberString(String num)");
+for (let i = mylist.length - 1; i > 0; i--) { 
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = mylist[i];
+    mylist[i] = mylist[j];
+    mylist[j] = temp;
+}
+console.log(mylist);
+for(const each of mylist){
+    const newLi = document.createElement("li");
+    newLi.textContent = each; 
+    document.getElementById("algorithmList").appendChild(newLi);
+}
+</script>
---
+++ src/StringLoopAlgorithms.java
@@ -0,0 +1,109 @@
+public class StringLoopAlgorithms {
+    // TODO DO THIS FIRST: get rid of vscode and use VSCodium instead, I don't want Microsoft to track my code😆
+    // Directions:
+    // Pick any 5 of the following algorithms and write static methods for them (ask Mr. Riley if you need clarification about how an algorithm should work)
+    // int removeOddDigits(String number)
+    // countTimesLetterAppears()
+    public static int countTimesLetterAppears(String word, String characterBeingCounted){
+            int numOfTimesLetterAppears = 0;
+            for(int i = 0; i < word.length(); i++){
+                String letter = word.substring(i, i + 1);
+                if(letter.equals(characterBeingCounted)){
+                    numOfTimesLetterAppears++;
+                }
+            }
+            return numOfTimesLetterAppears;
+    }
+
+    // boolean hasNumbers(String myString)
+    // String removeVowels(String myString)
+    // String sayLaXTimes(int x)
+    // double averageDigitsOfNumberString(String num)
+    // String removeLetter(String fullString, String letter)
+    // hasOnlyConsonants()
+    public static boolean hasOnlyConsonants(String word){
+            int numOfVowels = 0;
+            for(int i = 0; i < word.length(); i++){
+                String letter = word.substring(i, i + 1).toLowerCase();
+                if(letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u")){
+                    numOfVowels++;
+                }
+            }
+            if(numOfVowels > 0){
+                return false;
+            }
+            else {
+                return true;
+            }
+    }
+
+    // int sumDigitsOfNumberString(String num)
+    // int maxDigitOfNumberString(String num)
+    // countVowels()
+    public static int countVowels(String myString){
+            int numOfVowels = 0;
+            for(int i = 0; i < myString.length(); i++){
+                String letter = myString.substring(i, i + 1).toLowerCase();
+                if(letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u")){
+                    numOfVowels++;
+                }
+            }
+            return numOfVowels;
+    }
+
+    // boolean hasOnlyLetter(String fullString, String letter)
+    // doesLetterAppear()
+    public static boolean doesLetterAppear(String word, String booleanLetter){
+            int doesItAppear = 0;
+            for(int i = 0; i < word.length(); i++){
+                String letter = word.substring(i, i + 1);
+                if(letter.equals(booleanLetter)){
+                    doesItAppear++;
+                }
+            }
+            if(doesItAppear == 0){
+                return false;
+            }
+            else {
+                return true;
+            }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("--==++** Starting String Loop Algorithms Tests **++==--");
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                System.out.println("-=+* Testing countVowels *+=-");
+                    System.out.println("Expect: 2 from 'Apple' \nActual: " + countVowels("Apple"));
+                    System.out.println("Expect: 5 from 'Pomegranate' \nActual: " + countVowels("Pomegranate"));
+                    System.out.println("Expect: 4 from 'Gladiolus' \nActual: " + countVowels("Gladiolus"));
+                    System.out.println("Expect: 3 from 'Cordial' \nActual: " + countVowels("Cordial"));
+                    System.out.println("Expect: 8 from 'Onomatopoeia' \nActual: " + countVowels("Onomatopoeia"));
+                System.out.println("-=+* Finished testing countVowels *+=-");
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                System.out.println("-=+* Testing countTimesLetterAppears *+=-");
+                    System.out.println("Expect: 2 from 'apple', 'p' \nActual: " + countTimesLetterAppears("apple", "p"));
+                    System.out.println("Expect: 3 from 'banana', 'a' \nActual: " + countTimesLetterAppears("banana", "a"));
+                    System.out.println("Expect: 0 from 'hello', 'z' \nActual: " + countTimesLetterAppears("hello", "z"));
+                    System.out.println("Expect: 4 from 'Mississippi', 's' \nActual: " + countTimesLetterAppears("Mississippi", "s"));
+                    System.out.println("Expect: 0 from '', 'a' \nActual: " + countTimesLetterAppears("", "a"));
+                System.out.println("-=+* Finished testing countTimesLetterAppears *+=-");
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                System.out.println("-=+* Testing doesLetterAppear *+=-");
+                    System.out.println("Expect: true from 'Apple', 'p' \nActual: " + doesLetterAppear("Apple", "p"));
+                    System.out.println("Expect: false from 'Banana', 'z' \nActual: " + doesLetterAppear("Banana", "z"));
+                    System.out.println("Expect: false from 'Apple', 'a' \nActual: " + doesLetterAppear("Apple", "a"));
+                    System.out.println("Expect: true from 'Mississippi', 'i' \nActual: " + doesLetterAppear("Mississippi", "i"));
+                    System.out.println("Expect: false from '', 'e' \nActual: " + doesLetterAppear("", "e"));
+                System.out.println("-=+* Finished testing doesLetterAppear *+=-");
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                System.out.println("-=+* Testing hasOnlyConsonants *+=-");
+                    System.out.println("Expect: false from 'Apple' \nActual: " + hasOnlyConsonants("Apple"));
+                    System.out.println("Expect: true from 'Rhythm' \nActual: " + hasOnlyConsonants("Rhythm"));
+                    System.out.println("Expect: true from 'Gym' \nActual: " + hasOnlyConsonants("Gym"));
+                    System.out.println("Expect: true from 'Fly' \nActual: " + hasOnlyConsonants("Fly"));
+                    System.out.println("Expect: true from 'Ssh' \nActual: " + hasOnlyConsonants("Ssh"));
+                System.out.println("-=+* Finished testing hasOnlyConsonants *+=-");
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.println("--==++** Finished String Loop Algorithms Tests **++==--");
+    }
+}
